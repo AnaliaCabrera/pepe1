60203992958d3c0bc328fed4592ff54ced5cb21d
Lucas Chiesa <lucas.chiesa@gmail.com>
2018-01-07 21:12:37 +0100

    More cleanup and tests passing

--- d2b_tools/d2b_tools/measurements_reader.py
+++ d2b_tools/d2b_tools/measurements_reader.py
@@ -59,36 +59,105 @@ def get_counts(segment_header_separator, rightmost_angle, segment):
     return list(pixel_per_detector_generator(all_counts[2:], pixel_and_detector_amount))
 
 
+class Detector:
 
-class DetectorMeasurement:
+    def __init__(self, number, monitor, angle, counts):
+        self._number = number
+        self._angle = angle
+        self._counts = counts
+        self._monitor = monitor
+
+
+class Shot:
 
     SEGMENT_HEADER_SEPARATOR = 'I'*80
 
     # :param detector_angle_calibration: Angular distance from the rightmost detector to every detector.
     #     The list should have 128 values, with the last value being 0.
 
-
     @property
     def rightmost_angle(self):
         return self._metadata['anglesx1000']
 
+    @property
+    def monitor(self):
+        return self._metadata['monitor']
+
     @classmethod
-    def from_measurement_string(segment_string):
-        measurement = DetectorMeasurement()
-        measurement._metadata = get_metadata(self.SEGMENT_HEADER_SEPARATOR, segment_string)
-        measurement._counts = get_counts(self.SEGMENT_HEADER_SEPARATOR, segment_string, measurement.rightmost_angle)
-        return measurement
+    def from_measurement_string(cls, segment_string):
+        metadata = get_metadata(cls.SEGMENT_HEADER_SEPARATOR, segment_string)
+        shot = Shot(metadata)
+        shot_counts = get_counts(cls.SEGMENT_HEADER_SEPARATOR, shot.rightmost_angle, segment_string)
+        for number, counts in enumerate(shot_counts):
+            shot.add_detector_measurement(number, counts)
+        return shot
+
+    def __init__(self, metadata):
+        self._metadata = metadata
+        self._detectors = []
+        self._detector_angle_calibration = [-158.750, -157.500, -156.250, -155.000, -153.750, -152.500,
+                                            -151.250, -150.000, -148.750, -147.500, -146.250, -145.000,
+                                            -143.750, -142.500, -141.250, -140.000, -138.750, -137.500,
+                                            -136.250, -135.000, -133.750, -132.500, -131.262, -130.027,
+                                            -128.764, -127.501, -126.262, -124.986, -123.744, -122.488,
+                                            -121.250, -119.996, -118.749, -117.503, -116.240, -115.000,
+                                            -113.760, -112.510, -111.225, -110.011, -108.726, -107.537,
+                                            -106.240, -104.974, -103.739, -102.491, -101.254, -99.9934,
+                                            -98.7551, -97.5007, -96.2569, -94.9937, -93.7413, -92.5081,
+                                            -91.2396, -89.9824, -88.7602, -87.5170, -86.2329, -84.9932,
+                                            -83.7641, -82.5050, -81.2662, -79.9966, -78.7527, -77.5035,
+                                            -76.2564, -74.9872, -73.7477, -72.5086, -71.2457, -70.0003,
+                                            -68.7467, -67.4861, -66.2499, -65.0244, -63.7557, -62.5025,
+                                            -61.2484, -59.9862, -58.7416, -57.4992, -56.2372, -54.9913,
+                                            -53.7547, -52.4989, -51.2399, -50.0088, -48.7492, -47.4948,
+                                            -46.2517, -45.0034, -43.7366, -42.4932, -41.2465, -39.9866,
+                                            -38.7377, -37.5183, -36.2678, -35.0202, -33.7731, -32.4958,
+                                            -31.2734, -30.0018, -28.7291, -27.4887, -26.2319, -24.9728,
+                                            -23.7554, -22.4771, -21.2376, -20.0052, -18.7470, -17.5007,
+                                            -16.2531, -14.9891, -13.7492, -12.5056, -11.2798, -10.0067,
+                                            -8.73692, -7.50000, -6.25000, -5.00000, -3.75000, -2.50000,
+                                            -1.25000, 0.000000]
+
+    def add_detector_measurement(self, detector_number, counts):
+        angle = self.get_detector_angle(detector_number)
+        self._detectors.append(Detector(detector_number, self.monitor, angle, counts))
+
+    def get_detector_measurments(self):
+        return self._detectors
+
+    def set_detector_calibration(self, detector_calibration):
+        self._detector_angle_calibration = detector_calibration
 
     def get_detector_angle(self, detector_number):
-        return self.rightmost_angle - self.detector_angle_calibration[detector_number]
+        return self.rightmost_angle - self._detector_angle_calibration[detector_number]
+
+
 
 class MeasurementsReader:
 
     SEPARATOR_STRING = 'S'*80
     SEGMENTS_TO_SKIP = 1
 
+    def __init__(self):
+        self._shots = []
+
     def get_measurments_segments(self, file_data):
         return file_data.split(self.SEPARATOR_STRING)[self.SEGMENTS_TO_SKIP:]
 
     def read_file(self, file_name):
-        return [0]*25
+        with open(file_name, 'r') as infile:
+            file_data = infile.read()
+        shot_file_segments = self.get_measurments_segments(file_data)
+        for shot_segment in shot_file_segments:
+            self._shots.append(Shot.from_measurement_string(shot_segment))
+
+        return self._shots
+
+
+def integration_test():
+    mr = MeasurementsReader()
+    shots = mr.get_measurments_segments('../tests/data/sample_input.txt')
+    print (len(shots))
+
+if __name__ == '__main__':
+    integration_test()
--- d2b_tools/tests/test_measurements_reader.py
+++ d2b_tools/tests/test_measurements_reader.py
@@ -3,7 +3,7 @@ import unittest
 from d2b_tools import measurements_reader
 
 
-class TestDetectorMeasurement(unittest.TestCase):
+class TestUtilities(unittest.TestCase):
 
     def setUp(self):
         self._mr = measurements_reader.MeasurementsReader()
@@ -14,10 +14,9 @@ class TestDetectorMeasurement(unittest.TestCase):
             segments = self._mr.get_measurments_segments(file_data)
         return segments[segment_index]
 
-
     def test_get_metadata(self):
         segment = self.read_sample_segment()
-        metadata = measurements_reader.get_metadata(measurements_reader.DetectorMeasurement.SEGMENT_HEADER_SEPARATOR,
+        metadata = measurements_reader.get_metadata(measurements_reader.Shot.SEGMENT_HEADER_SEPARATOR,
                                                     segment)
         self.assertTrue('Total_Cou' in metadata)
         self.assertTrue('monitor' in metadata)
@@ -27,7 +26,7 @@ class TestDetectorMeasurement(unittest.TestCase):
 
     def test_get_counts(self):
         segment = self.read_sample_segment()
-        counts = measurements_reader.get_counts(measurements_reader.DetectorMeasurement.SEGMENT_HEADER_SEPARATOR,
+        counts = measurements_reader.get_counts(measurements_reader.Shot.SEGMENT_HEADER_SEPARATOR,
                                                 1.31246000e5, segment)
         self.assertEqual(len(counts), 128)
         self.assertEqual(len(counts[0]), 128)
@@ -36,6 +35,10 @@ class TestDetectorMeasurement(unittest.TestCase):
         test_list = [0,1,2,5,4,3,6,7,8,11,10,9]
         output = list(measurements_reader.pixel_per_detector_generator(test_list, 3))
         self.assertListEqual(output, [[0,1,2], [3,4,5], [6,7,8], [9,10,11]])
+
+
+class TestShot(unittest.TestCase):
+    pass
 
 
 class TestMeasurementReader(unittest.TestCase):
