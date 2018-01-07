ae3067b5dc057571357c9a433201015ab25f2cc1
Lucas Chiesa <lucas.chiesa@gmail.com>
2018-01-07 15:55:09 +0100

    Start python package

---
+++ README.md
@@ -0,0 +1,18 @@
+# Installation
+
+1. Create a python 3 virtualenv
+```
+$ pyvenv d2benv
+```
+2. Activate the new environment
+```
+$ source d2benv/bin/activate
+```
+3. Install the dependencies
+```
+$ pip install -r requirements.txt
+```
+4. Install the d2b_tools package
+```
+$ pip install -e .
+```
---
+++ d2b_tools/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='d2b_tools',
+      version='0.1',
+      description='D2B python library and tools',
+      author='Analia Cabrera',
+      author_email='saltacabrera@gmail.com',
+      packages=['d2b_tools'],
+      #scripts=['scripts']
+     )
